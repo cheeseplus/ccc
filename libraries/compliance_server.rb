806110aab8484731f15f967ab8ca6c68563b7501
Alex Pop <al3xpop@gmail.com>
2016-02-01 09:46:53 +0000

    moved the check earlier

--- libraries/compliance_server.rb
+++ libraries/compliance_server.rb
@@ -16,33 +16,33 @@ class ComplianceServer < Chef::Resource
   default_action :install
 
   action :install do
-    converge_by 'Install Chef Compliance' do
-      if package_url
-        pkgname = ::File.basename(package_url)
-        cache_path = ::File.join(Chef::Config[:file_cache_path], pkgname).gsub(/~/, '-')
-        Chef::Log.warn "*** Downloading Chef Compliance package '#{pkgname}' for action '#{action}'"
+    path = '/opt/chef-compliance/bin/chef-compliance-ctl'
+    if action == :upgrade || !::File.exist?(path)
+      converge_by 'Install Chef Compliance' do
+        if package_url
+          pkgname = ::File.basename(package_url)
+          cache_path = ::File.join(Chef::Config[:file_cache_path], pkgname).gsub(/~/, '-')
+          Chef::Log.warn "*** Downloading Chef Compliance package '#{pkgname}' for action '#{action}'"
 
-        remote_file cache_path do
-          source package_url
-          mode '0644'
+          remote_file cache_path do
+            source package_url
+            mode '0644'
+            action :create
+          end
+        end
+
+        case node['platform_family']
+        when 'debian'
+          node.default['apt-chef']['repo_name'] = package_repo
+        when 'rhel'
+          node.default['yum-chef']['repositoryid'] = package_repo
+        end
+
+        directory '/etc/chef-compliance' do
+          recursive true
           action :create
         end
-      end
 
-      case node['platform_family']
-      when 'debian'
-        node.default['apt-chef']['repo_name'] = package_repo
-      when 'rhel'
-        node.default['yum-chef']['repositoryid'] = package_repo
-      end
-
-      directory '/etc/chef-compliance' do
-        recursive true
-        action :create
-      end
-
-      # Can't use not_if for chef_ingredient due to the array of actions
-      if action == :upgrade || !::File.exist?('/opt/chef-compliance/bin/chef-compliance-ctl')
         ### TODO: get content from config hash
         file '/etc/chef-compliance/chef-compliance.rb' do
           owner 'root'
@@ -70,29 +70,30 @@ class ComplianceServer < Chef::Resource
           notifies :create, "compliance_user[#{admin_user}]"
           action actions_array
         end
-      end
 
-      # Custom resource defined in this cookbook
-      compliance_user admin_user do
-        password admin_pass
-        only_if do
-          admin_user.is_a?(String) && admin_user.length > 0 &&
-            admin_pass.is_a?(String) && admin_pass.length > 0
+        # Custom resource defined in this cookbook
+        compliance_user admin_user do
+          password admin_pass
+          only_if do
+            admin_user.is_a?(String) && admin_user.length > 0 &&
+              admin_pass.is_a?(String) && admin_pass.length > 0
+          end
+          notifies :restart, 'omnibus_service[compliance/core]'
+          action :nothing
         end
-        notifies :restart, 'omnibus_service[compliance/core]'
-        action :nothing
-      end
 
-      # Custom resource from the `chef-ingredient` cookbook
-      omnibus_service 'compliance/core' do
-        action :nothing
+        # Custom resource from the `chef-ingredient` cookbook
+        omnibus_service 'compliance/core' do
+          action :nothing
+        end
       end
+    else
+      Chef::Log.warn "#{path} already exists, skipping install..."
     end
   end
 
   action :upgrade do
     converge_by 'Upgrade Chef Compliance' do
-      Chef::Log.warn "*** Upgrade Chef Compliance from #{package_url} or #{package_repo} for action #{action}"
       action_install
     end
   end
